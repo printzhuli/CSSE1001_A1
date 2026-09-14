43dcdc18e0020857d6c525a1f096260c5f8fbfff
JC <64360683+printzhuli@users.noreply.github.com>
2026-09-14 14:16:27 +1000

    Add CSSE1001 assignment files

---
+++ a1.py
@@ -0,0 +1,19 @@
+# DO NOT modify or add any import statements
+from support import *
+from pokemon import POKEMON
+
+# Name:
+# Student Number: 
+# Least favourite Pokemon: 
+# -----------------------------------------------------------------------------
+
+# Define your classes and functions here
+def main(sol_index: int):
+   pass
+
+if __name__ == "__main__":
+    # Run with your choice of Pokemon (This section is for your own testing)
+    main(256)
+
+
+
---
+++ support.py
@@ -0,0 +1,21 @@
+GREEN = "O"
+YELLOW = "~"
+RED = "X"
+EMPTY = "-"
+
+QUIT = 'q'
+HELP = 'h'
+HINT = 't'
+
+WELCOME_MSG = "Welcome to Pokele"
+LOSE_MSG = "You fainted!"
+WIN_MSG = "You won this battle of wits!"
+HELP_MSG = """Type the name of Pokemon to enter a guess, OR:
+- [h/H]: Display this help message
+- [q/Q]: Quit the game
+- [t/T]: Request a hint"""
+NO_HINT_MSG = "You have used all hints!"
+INVALID_HINT_MSG = "You have already guessed all letters!"
+REMAINING_MESSAGE = "Hints remaining."
+
+GUESS_PROMPT = "Please enter your guess (or h for help): "
